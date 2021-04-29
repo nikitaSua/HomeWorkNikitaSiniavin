fa12ac8f7df1c3ee3f13c82f3ae906366bb20b90
NikitaSua <62516130+nikitaSua@users.noreply.github.com>
2021-04-30 01:48:58 +0300

    Задание_1 V_1

---
+++ Task_1/Parsers/FileParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1.FileTypes;
+
+namespace Task_1.Parsers
+{
+    public class FileParser
+    {
+
+        private TextParser Tp = new TextParser();
+        private ImageParser Ip = new ImageParser();
+        private MovieParser Mp = new MovieParser();
+
+
+        public List<AFile> Parse(string parsedStr)
+        {
+            List<AFile> files= new List<AFile>();
+            string[] separatedStr = parsedStr.Split("\n");
+            for (int i = 0; i < separatedStr.Length; i++)
+            {
+                if (separatedStr[i].Contains("Text"))
+                {
+                    files.Add(Tp.Parse(separatedStr[i]));
+                }
+                if (separatedStr[i].Contains("Movie"))
+                {
+                    files.Add(Mp.Parse(separatedStr[i]));
+                }
+                if (separatedStr[i].Contains("Image"))
+                {
+                    files.Add(Ip.Parse(separatedStr[i]));
+                }
+                
+            }
+            return files;
+        }
+
+
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Task_1.FileTypes;
+using Task_1.Parsers;
+using System.Linq;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FileParser a = new FileParser();
+            string inputSTR = "Text:file.txt(6B);Some string content\n Image:img.bmp(19MB);1920х1080\n" +
+                "Text:data.txt(12B);Another string\n Text:data1.txt(7B);Yet another string\n" +
+                "Movie:logan.2017.mkv(19GB);1920х1080;2h12m";
+
+            List<AFile> files = a.Parse(inputSTR);
+
+
+            var sortedText = from f in files
+                             where f.Extension=="txt"
+                              orderby f.ConvertedSize
+                              select f;
+            var sortedImage = from f in files
+                             where f.Extension == "bmp"
+                              orderby f.ConvertedSize
+                             select f;
+            var sortedMovie = from f in files
+                              where f.Extension == "mkv"
+                              orderby f.ConvertedSize
+                              select f;
+
+            Console.WriteLine("Text files:");
+            foreach (var item in sortedText)
+            {
+                item.Display();
+            }
+            Console.WriteLine("Movies:");
+            foreach (var item in sortedMovie)
+            {
+                item.Display();
+            }
+            Console.WriteLine("Images:");
+            foreach (var item in sortedImage)
+            {
+                item.Display();
+            }
+
+
+        }
+    }
+}
