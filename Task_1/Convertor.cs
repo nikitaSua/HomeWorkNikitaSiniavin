66aecb83147d9f28c8686b3859867257e40c2e3e
NikitaSua <62516130+nikitaSua@users.noreply.github.com>
2021-04-30 01:52:47 +0300

    Задание 1 В_1

---
+++ Task_1/Convertor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    static class Convertor
+    {
+        public static decimal MyConvert(string str)
+        {
+            int vul = Convert.ToInt32(stringWorker.SizeIntSearcher(str));
+            string format = stringWorker.SizeFormatSearcher(str);
+            if (format== "MB")
+            {
+                return (decimal)vul;
+            }
+            if (format == "B")
+            {
+
+                return vul * 0.000001m;
+            }
+            if (format == "GB")
+            {
+                return vul *1024;
+            }
+            else
+            {
+                return 0;
+            }
+
+            
+        }
+    }
+}
---
+++ Task_1/stringWorker.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public static class stringWorker
+    {
+        public static string SizeSearcher(string str)
+        {
+            string pattern = @"\(\w*\)";
+            string pattern_brackets = @"\(|\)";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern_brackets).Replace(val, "");
+            return val;
+        }
+
+        
+        public static string SizeFormatSearcher(string str)
+        {
+            string pattern = @"\D+";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            return val;
+        }
+        public static string SizeIntSearcher(string str)
+        {
+            string pattern = @"\d*";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            return val;
+        }
+
+
+        public static string NameSearcher(string str)//NameSearcher
+        {
+            string pattern = @":.*\(";
+            string pattern2 = @":|\(";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern2).Replace(val, "");
+            return val;
+        }
+        public static string ExtensionSearcher(string str)
+        {
+            
+            string pattern = @"\.\w*\(";
+            string pattern2 = @"\.|\(";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern2).Replace(val, "");
+            return val;
+        }
+        public static string ResolutionSearcher(string str)
+        {
+            string pattern = @";\d*\w\d*";
+            string pattern2 = @";";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern2).Replace(val, "");
+            return val;
+        }
+        public static string DurationSearcher(string str)//DurationSearcher
+        {
+            string pattern = @"\w*$";
+            string pattern2 = @";";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern2).Replace(val, "");
+            return val;
+        }
+        public static string ContentSearcher(string str)
+        {
+            string pattern = @";.*$";
+            string pattern2 = @";";
+            var temp = Regex.Match(str, pattern);
+            string val = temp.Value;
+            val = new Regex(pattern2).Replace(val, "");
+            return val;
+        }
+
+    }
+}
