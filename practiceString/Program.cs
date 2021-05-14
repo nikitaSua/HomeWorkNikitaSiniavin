dde487956d20e3d1faa07d94dd1bfa54cfee8ce7
NikitaSua <62516130+nikitaSua@users.noreply.github.com>
2021-05-14 20:11:30 +0300

    Доделал задания

--- practiceString/Program.cs
+++ practiceString/Program.cs
@@ -16,6 +16,18 @@ namespace practiceString
             StringWorker.StringDateToDate("2016 21-07");
             StringWorker.IntNumbersStingSumParse("1,2,3");
             StringWorker.RegExAnyLettersAnyNims("avc123, arw456.");
+            StringWorker.RegExUserPasswordValidatorSixSigns("Aa3bvccc");
+            StringWorker.RegExUserPostCodeValidator("123-123");
+            StringWorker.RegExPhoneNumberValidator("+380-98-123-45-67");
+            StringWorker.RegExPhoneNumberReplaser("+380-98-123-42-11");
+            StringWorker.FirstNameLatterReplaser(new string[] { "иван иванов", "светлана иванова-петренко" });
+            StringWorker.DecoderBae64("0JXRgdC70Lgg0YLRiyDRh9C40YLQsNC10YjRjCDRjdGC0L7RgiDRgtC10LrRgdGCLCDQt9C90LDRh9C40YIg0LfQsNC00LDQvdC40LUg0LLRi9C/0L7Qu9C90LXQvdC+INCy0LXRgNC90L4gOik=");
+            
+            double[] arr = { 9, 1.5, 34.4, 234, 1, 56.5 };
+            StringWorker.quicksort<double>(arr, 0, arr.Length - 1);
+
+
+
         }
     }
 }
--- practiceString/StringWorker.cs
+++ practiceString/StringWorker.cs
@@ -21,7 +21,7 @@ namespace practiceString
             {
                 var temp = Regex.Match(str[i].ToString(), pattern);
                 string val = temp.Value;
-                if (val!="")
+                if (val != "")
                 {
                     numbers.Add(Convert.ToInt32(val));
                 }
@@ -59,10 +59,10 @@ namespace practiceString
             string LastValue = LastPArtTemp.Value;
             string SignTempval = SignTemp.Value;
 
-            int answerA=0;
+            int answerA = 0;
 
 
-            if (SignTempval=="")
+            if (SignTempval == "")
             {
                 for (int i = 0; i < Convert.ToInt32(LastValue); i++)
                 {
@@ -86,7 +86,7 @@ namespace practiceString
 
         public static void StringDateToDate(string str)
         {
-            
+
             DateTime Arr = DateTime.ParseExact(str, "yyyy dd-MM", CultureInfo.InvariantCulture);
 
             Console.WriteLine($"{Arr.Year} {Arr.Day}-{Arr.Month}");
@@ -94,12 +94,12 @@ namespace practiceString
 
         public static void IntNumbersStingSumParse(string str)
         {
-            int totulSum=0;
+            int totulSum = 0;
             string[] nums = str.Split(",");
 
             foreach (var item in nums)
             {
-                totulSum+=Convert.ToInt32(item);
+                totulSum += Convert.ToInt32(item);
             }
             Console.WriteLine(totulSum);
         }
@@ -112,10 +112,10 @@ namespace practiceString
 
             foreach (var item in splitedStrings)
             {
-                
+
                 var temp = Regex.Match(item, pattern);
                 string val = temp.Value;
-                if (val!="")
+                if (val != "")
                 {
                     result.Add(val);
                 }
@@ -128,11 +128,108 @@ namespace practiceString
 
         }
 
-        
+
         public static void RegExUserPasswordValidatorSixSigns(string str)
         {
+            string pattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$";
+
+            if (Regex.IsMatch(str, pattern))
+            {
+                Console.WriteLine("Password is ok");
+            }
+            else Console.WriteLine("Wrong Password format");
+        }
+        public static void RegExUserPostCodeValidator(string str)
+        {
+            string pattern = @"[\d]{3}-[\d]{3}";
+
+            if (Regex.IsMatch(str, pattern))
+            {
+                Console.WriteLine("Post Code is ok");
+            }
+            else Console.WriteLine("Wrong Post Code format");
+        }
+
+        public static void RegExPhoneNumberValidator(string str)
+        {
+            string pattern = ("[+][0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{2}-[0-9]{2}");
+            if (Regex.IsMatch(str, pattern))
+            {
+                Console.WriteLine("Phone number is ok");
+            }
+            else Console.WriteLine("Wrong phone number format");
+        }
+
+        public static void RegExPhoneNumberReplaser(string str)
+        {
+            string pattern = ("[+][0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{2}-[0-9]{2}");
+            string target = "+XXX-XX-XXX-XX-XX";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(str, target);
+
+            Console.WriteLine(result);
+        }
+
+        public static void FirstNameLatterReplaser(string[] str)
+        {
+            string pattern = (@"^\w*");
+
+            var textInfo = new CultureInfo("ru-RU").TextInfo;
+            List<string> UpperNames = new List<string>();
+
+            foreach (string item in str)
+            {
+                var capitalizedText = textInfo.ToTitleCase(textInfo.ToLower(item));
+                UpperNames.Add(capitalizedText);
+            }
+            foreach (var item in UpperNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void DecoderBae64(string str)
+        {
+            var Converted = Convert.FromBase64String(str);
+
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(Converted));
 
         }
+
+
+        static int partition<T>(T[] m, int a, int b)
+        where T : IComparable<T>
+        {
+            int i = a;
+            for (int j = a; j <= b; j++)         // просматриваем с a по b
+            {
+                if (m[j].CompareTo(m[b]) <= 0)  // если элемент m[j] не превосходит m[b],
+                {
+                    T t = m[i];                  // меняем местами m[j] и m[a], m[a+1], m[a+2] и так далее...
+                    m[i] = m[j];                 // то есть переносим элементы меньшие m[b] в начало,
+                    m[j] = t;                    // а затем и сам m[b] «сверху»
+                    i++;                         // таким образом последний обмен: m[b] и m[i], после чего i++
+                }
+            }
+            return i - 1;                        // в индексе i хранится <новая позиция элемента m[b]> + 1
+        }
+
+
+        public static void quicksort<T>(T[] m, int a, int b) where T : IComparable<T>// a - начало подмножества, b - конец
+        {                                        // для первого вызова: a = 0, b = <элементов в массиве> - 1
+            if (a >= b) return;
+            int c = partition(m, a, b);
+            quicksort(m, a, c - 1);
+            quicksort(m, c + 1, b);
+        }
+    }
+
+            
+
+    }
+
+
+
 
     }
 }
