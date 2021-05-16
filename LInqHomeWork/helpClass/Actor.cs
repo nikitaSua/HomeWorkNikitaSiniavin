fa315dbe8d10d5d81e450eaced646be34e811688
NikitaSua <62516130+nikitaSua@users.noreply.github.com>
2021-05-17 01:17:19 +0300

    Add files via upload

---
+++ LInqHomeWork/helpClass/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInqHomeWork.helpClass
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+}
---
+++ LInqHomeWork/helpClass/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInqHomeWork.helpClass
+{
+    public class Film : ArtObject
+    {
+
+        public int Length { get; set; }
+        public IEnumerable<Actor> Actors { get; set; }
+    }
+
+}
