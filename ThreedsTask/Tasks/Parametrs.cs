a90e08ba6476fe0827e7f7ac43b3e8ce00a15780
NikitaSua <62516130+nikitaSua@users.noreply.github.com>
2021-05-25 02:49:10 +0300

    Задание по потокам
    Environment.ProcessorCount

---
+++ ThreedsTask/Tasks/Parametrs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreedsTask.Tasks
+{
+    public class Parametrs
+    {
+        public int start;
+        public int end;
+        public Parametrs(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
---
+++ ThreedsTask/Tasks/RandValueNums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreedsTask
+{
+    public class RandValueNums :ITask
+    {
+        public void Execute()
+        {
+            Random rnd = new Random();
+            int[] resultMas = new int[100];
+            for (int i = 0; i < resultMas.Length; i++)
+            {
+                resultMas[i] = rnd.Next(0, 10);
+            }
+            foreach (var item in resultMas)
+            {
+                Console.WriteLine( string.Join(" ; ", item));
+            }
+        }
+    }
+}
